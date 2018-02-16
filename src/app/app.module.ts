b6947d61f6e4fe0e4264c3265c6d21ebc82fd68e
Nana <s58888@beuth-hochschule.de>
2018-02-16 10:21:26 +0100

    install plugin, get magnetic Heading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { DeviceOrientation } from '@ionic-native/device-orientation';
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    DeviceOrientation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,45 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, Input } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import { DeviceOrientation, DeviceOrientationCompassHeading } from '@ionic-native/device-orientation';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor(public navCtrl: NavController) {
+  @Input() magneticHeading: number;
+  constructor(public navCtrl: NavController, private deviceOrientation: DeviceOrientation, private platform: Platform) {
 
+  }
+
+  ngOnInit() {
+    if (this.platform.is('cordova')) {
+      console.log('You are on a device, cordova plugins are accessible');
+    } else {      
+      console.log('Cordova not accessible, add mock data if necessary');
+    }
+  }
+
+  getMagHeading() {
+    console.log('click');
+    // Get the device current compass heading
+    this.deviceOrientation.getCurrentHeading().then(
+    (data: DeviceOrientationCompassHeading) => {
+      console.log(data);
+      this.magneticHeading = data.magneticHeading;      
+    },
+    (error: any) => console.log(error)
+    );
+    
+    // Watch the device compass heading change
+    var subscription = this.deviceOrientation.watchHeading().subscribe(
+    (data: DeviceOrientationCompassHeading) => console.log(data)
+    );
+    
+    // Stop watching heading change
+    subscription.unsubscribe();
   }
 
 }
